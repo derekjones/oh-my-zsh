ced2bfdf9be8aa7bd5f1190db8511e5083c7aff5
Derek Jones <derek.jones@ellislab.com>
2016-04-05 17:17:48 -0700

    adding php-fpm version switching

--- custom/derekjones.zsh
+++ custom/derekjones.zsh
@@ -1,7 +1,7 @@
 alias l="ls -fla"
 
 # Customize to your needs...
-export PATH=/usr/local/php5/bin:/Users/derek/bin:/usr/local/bin:/usr/local/git/bin:Users/derek/pear/bin:/Users/derek/Library/Haskell/bin:$PATH
+export PATH=/usr/local/php5/bin:/Users/derek/bin:/usr/local/bin:/usr/local/sbin:/usr/local/git/bin:$PATH
 
 
 WEB_DIR_HOME="${HOME}/Sites"
@@ -31,6 +31,39 @@ ht()
 	fi
 }
 
+alias restart-fpm="nocorrect restart-fpm"
+
+restart-fpm()
+{
+	if [ $# -ne 2 ]; then
+		echo "usage: restart-fpm verstop verstart";echo;
+		echo "    verstop    one of: 53 54 55 56 70";
+		echo "    verstart   one of: 53 54 55 56 70"; echo;
+		return
+	fi
+
+	echo "Stopping php-fpm $1..."
+	launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.php$1.plist
+	echo "Starting php-fpm $2..."
+	launchctl load -w   ~/Library/LaunchAgents/homebrew.mxcl.php$2.plist
+	echo "php-fpm restarted"
+}
+
+alias sphp="nocorrect sphp"
+sphp()
+{
+	if [ $# -ne 2 ]; then
+		echo "usage: sphp from to";echo;
+		echo "    from    one of: 53 54 55 56 70";
+		echo "    to      one of: 53 54 55 56 70"; echo;
+		return
+	fi
+
+	brew unlink php$1
+	brew link php$2
+	restart-fpm $1 $2
+}
+
 chrome-ellislab()
 {
 	/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --proxy-server=localhost:3128 2>&1 &
@@ -41,15 +74,15 @@ function parse_git_branch {
   echo "("${ref#refs/heads/}")"
 }
 
-# Phing
-#export PHP_COMMAND=/usr/bin/php
-export PHP_COMMAND=/usr/local/php5/bin/php
-export PHING_HOME=/usr/local/phing
-export PHP_CLASSPATH=${PHING_HOME}/classes
-export PATH=${PATH}:${PHING_HOME}/bin
-
-# Python
-export PATH=PATH="/Library/Frameworks/Python.framework/Versions/2.7/bin:${PATH}"
-export PYTHONPATH='/Users/derek/Library/Python/2.7/site-packages'
-
 PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+
+# Some Phing stuff
+PHP_COMMAND=/usr/local/Cellar/php56/5.6.9/bin
+PHING_HOME=/usr/local/Cellar/php56/5.6.9/bin
+PHP_CLASSPATH=${PHING_HOME}/classes
+PATH=${PATH}:${PHING_HOME}
+
+# Boot2Docker stuff
+export DOCKER_HOST=tcp://192.168.59.103:2376
+export DOCKER_CERT_PATH=/Users/derek/.boot2docker/certs/boot2docker-vm
+export DOCKER_TLS_VERIFY=1
